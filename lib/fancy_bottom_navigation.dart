b18ee11b3ee945d5de046fa1183adfe9d9ee1cb2
m_molaei <m_molaei>
2022-12-02 18:43:12 +0330

    add custom circle color clipper

--- lib/fancy_bottom_navigation.dart
+++ lib/fancy_bottom_navigation.dart
@@ -41,8 +41,8 @@ class FancyBottomNavigation extends StatefulWidget {
 }
 
 class FancyBottomNavigationState extends State<FancyBottomNavigation> with TickerProviderStateMixin, RouteAware {
-  Widget nextIcon = Container(child: Icon(Icons.search));
-  Widget activeIcon = Container(child: Icon(Icons.search));
+  Widget nextIcon = Icon(Icons.search);
+  Widget activeIcon = Icon(Icons.search);
 
   int currentSelected = 0;
   double _circleAlignX = 0;
